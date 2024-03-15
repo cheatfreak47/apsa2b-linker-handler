3bb990fdbf1b8814a5e61edd9ddf191cc143b9e2
CheatFreak <cheatfreak47@gmail.com>
2024-03-15 07:59:30 -0400

    Add a mitigation for the port not being populated yet
    this happens on newly genned rooms on ultra speed connections where your room loads before the port can be chosen, so we refresh if the port is 0 till we get a valid port (responsibly after a 3 sec delay)

--- APSA2BLinker.user.js
+++ APSA2BLinker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Archipelago SA2B Linker
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Add an apsa2b:// URL to be used with the URL handler program.
 // @author       CheatFreak
 // @match        https://archipelago.gg/room/*
@@ -21,16 +21,26 @@
             // Check if the <tr> contains "Sonic Adventure 2 Battle" in the third <td>
             var game = $(this).find('td:nth-child(3)').text().trim();
             if (game === 'Sonic Adventure 2 Battle') {
-                // Check if the <tr> contains a <td> with the specific text
-                var td = $(this).find('td:contains("No file to download for this game.")');
-                if (td.length > 0) {
-                    // Find the <a> element with the archipelago protocol within the same <tr>
-                    var link = $(this).find('a[href^="archipelago://"]');
-                    if (link.length > 0) {
-                        // Replace the protocol in the href attribute
-                        var href = link.attr('href').replace('archipelago://', 'apsa2b://');
-                        // Replace the <td> contents with the new link
-                        td.html('<a href="' + href + '">Run SA2B Mod...</a>');
+                // Find the <a> element with the archipelago protocol within the same <tr>
+                var link = $(this).find('a[href^="archipelago://"]');
+                if (link.length > 0) {
+                    // Extract the port number from the href attribute
+                    var port = link.attr('href').split(':').pop();
+                    // Check if the port number is 0
+                    if (port !== '0') {
+                        // Wait 5 seconds before refreshing the page
+                        setTimeout(function() {
+                            location.reload();
+                        }, 3000);
+                    } else {
+                        // Check if the <tr> contains a <td> with the specific text
+                        var td = $(this).find('td:contains("No file to download for this game.")');
+                        if (td.length > 0) {
+                            // Replace the protocol in the href attribute
+                            var href = link.attr('href').replace('archipelago://', 'apsa2b://');
+                            // Replace the <td> contents with the new link
+                            td.html('<a href="' + href + '">Run SA2B Mod...</a>');
+                        }
                     }
                 }
             }
